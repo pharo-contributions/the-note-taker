99f9fe9734a2bd855de34c4010f61a1d934011bc
Quentin Moutté <111527926+moufort@users.noreply.github.com>
2025-07-10 14:23:11 +0200

    Create buildOSX.sh

---
+++ buildOSX.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+set -e
+
+wget -O -  get.pharo.org/64/110+vm | bash
+./pharo Pharo.image save TNT
+
+if [ ! -z "$GITHUB_WORKSPACE" ] 
+then
+	./pharo TNT.image eval --save "Metacello new 
+	    baseline:'TheNoteTaker'; 
+	    repository: 'gitlocal://$GITHUB_WORKSPACE/src';
+		onUpgradeUseLoaded;
+		onConflictUseLoaded;
+	    load'"
+else
+	./pharo TNT.image eval --save "Metacello new
+	    baseline:'TheNoteTaker';
+	    repository: 'github://moufort/the-note-taker/src';
+		onUpgradeUseLoaded;
+		onConflictUseLoaded;
+	    load"
+fi
+
+./pharo TNT.image eval "SpApplication generateOSXPackage"
+
+cd build
+chmod +x build.sh
+./build.sh
