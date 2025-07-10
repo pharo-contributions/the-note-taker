7d05c2e192b8d53e2ec22626d046b6bc1df6ad6e
Quentin Moutté <111527926+moufort@users.noreply.github.com>
2025-07-10 17:01:24 +0200

    Update buildOSX.sh

--- buildOSX.sh
+++ buildOSX.sh
@@ -7,7 +7,7 @@ wget -O -  get.pharo.org/64/130+vm | bash
 
 ./pharo Pharo.image metacello install github://hernanmd/the-note-taker/src BaselineOfTheNoteTaker --groups=Release;
 
-./pharo Pharo.image eval "NTCommandLineHandler generateApplication";
+./pharo Pharo.image --no-default-preferences eval "NTCommandLineHandler generateApplication";
 
 cd build
 chmod +x build.sh
