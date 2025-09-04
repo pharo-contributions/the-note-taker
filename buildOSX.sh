d9da3962f694030f25b683e4170575f98d732fd1
Quentin Moutté <111527926+moufort@users.noreply.github.com>
2025-09-04 12:06:29 +0200

    Update buildOSX.sh

--- buildOSX.sh
+++ buildOSX.sh
@@ -7,6 +7,7 @@ wget -O -  get.pharo.org/64/130+vm | bash
 
 ./pharo Pharo.image st scripts/preLoading.st --save --quit;
 ./pharo Pharo.image metacello install tonel://$GITHUB_WORKSPACE/src BaselineOfTheNoteTaker --groups=Release;
+./pharo Pharo.image eval "(IceRepositoryCreator new location: $GITHUB_WORKSPACE asFileReference; createRepository) register.";
 
 ./pharo Pharo.image eval "NTCommandLineHandler generateApplication";
 
