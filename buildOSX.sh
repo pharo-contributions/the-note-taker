41a65569d4ce014bdea301c188efc3b575b9b10f
Quentin Moutté <111527926+moufort@users.noreply.github.com>
2025-09-04 11:01:01 +0200

    Update buildOSX.sh

--- buildOSX.sh
+++ buildOSX.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 wget -O -  get.pharo.org/64/130+vm | bash
-
+tree
 ./pharo Pharo.image st scripts/preLoading.st --save --quit;
 ./pharo Pharo.image metacello install github://$GITHUB_REPOSITORY/src BaselineOfTheNoteTaker --groups=Release;
 
